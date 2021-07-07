06b45f4b036c226c694330662ca428fec3eef8d0
Matheus Domingos <matheusyurisd@gmail.com>
2021-07-07 15:29:31 -0300

    Add Jason encoder

--- lib/firstAppV2/accounts/user.ex
+++ lib/firstAppV2/accounts/user.ex
@@ -1,14 +1,13 @@
 defmodule FirstAppV2.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-
+  @derive {Jason.Encoder, except: [:__meta__]}
   schema "users" do
     field :name, :string
     field :email, :string
     field :age, :integer
     timestamps()
   end
-
   def changeset(user, attrs) do
     user
     |> cast(attrs, [:name, :email, :age])
