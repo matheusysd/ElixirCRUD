d78daf667c33ea96d285b50fada84c6b71eaac89
Matheus Domingos <matheusyurisd@gmail.com>
2021-07-07 21:07:42 -0300

    Change pattern matching for show.json views

--- lib/firstAppV2_web/views/user_view.ex
+++ lib/firstAppV2_web/views/user_view.ex
@@ -1,7 +1,10 @@
 defmodule FirstAppV2Web.UserView do
   use FirstAppV2Web, :view
-  def render("getUsers.json", %{data: data}) do
-    %{response: data}
+  def render("show.json", %{list_users: list_users}) do
+    %{response: list_users}
+  end
+  def render("show.json", %{user: user}) do
+    %{response: user}
   end
   def render("success.json", %{message: message}) do
     %{response: message}
@@ -10,3 +13,7 @@ defmodule FirstAppV2Web.UserView do
     %{message: message}
   end
 end
+
+
+# def render(list_users)
+# def render(%{user: user})
