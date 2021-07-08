fc4b88991e3c9c8104dac0755cd44ce0ef1baa5b
Matheus Domingos <matheusyurisd@gmail.com>
2021-07-07 22:39:15 -0300

    Comentarios e TODO

--- lib/firstAppV2_web/controllers/user_controller.ex
+++ lib/firstAppV2_web/controllers/user_controller.ex
@@ -1,28 +1,29 @@
 defmodule FirstAppV2Web.UserController do
   use FirstAppV2Web, :controller
 
-  alias FirstAppV2.Accounts
+  alias FirstAppV2.Accounts #Importando (?) arquivo onde estão as funções que trabalham com o db
 
   def get_users(conn, _params) do
     users = Accounts.get_users()
     users_infos = Enum.map(users, fn user -> Map.take(user, [:name, :id, :age, :email]) end)
+    # Iterando sobre a lista, para cada uma delas extraio as informações que quero
     render(conn, "show.json", list_users: users_infos)
   end
 
   def create_user(conn, params) do
     case Accounts.create_user(params) do
       {:ok, user} ->
-        user_id = Map.get(user, :id)
+        user_id = Map.get(user, :id) #Extraindo informação necessária
 
-        render(conn, "success.json", %{message: "Usuário criado com sucesso com o id: #{user_id}"})
+        render(conn, "success.json", %{message: "Usuário criado com sucesso com o id: #{user_id}"})# #{} semelhante ao template literals
 
-      {:error, _error} ->
+      {:error, _error} -> #_variavel não será usada
         render(conn, "error.json", %{message: "Erro ao criar usuário"})
     end
   end
 
   def get_user(conn, %{"id" => id}) do
-    user = Accounts.get_user(id)
+    user = Accounts.get_user(id) #não tem validação quando não acha o usuário
     user_info = Map.take(user, [:name, :id, :age, :email])
     render(conn, "show.json", user: user_info)
   end
@@ -39,7 +40,7 @@ defmodule FirstAppV2Web.UserController do
     end
   end
 
-  def update_user(conn, %{"id" => id, "user" => new_user_params}) do
+  def update_user(conn, %{"id" => id, "user" => new_user_params}) do #necessita receber um map com {id: e user:}
     user = Accounts.get_user(id)
 
     case Accounts.update_user(user, new_user_params) do
